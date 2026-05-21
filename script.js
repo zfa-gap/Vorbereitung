776b3901255509ca8b83488cf4923b98fa7f3471
zfa-gap <zfa.anmeldung.vorbereitung@gmail.com>
2026-05-21 16:53:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ const questions = {
 
   PV: [
 
+  
     {
       question:
       "Frau Meier ist bei der AOK versichert. Welche beiden Aussagen zur AOK sind richtig?",
@@ -64,7 +65,323 @@ const questions = {
       ],
 
       correct: [4]
-    }
+    },
+
+    {
+      question:
+      "Welche der nachfolgenden Krankenkassen gehören zu den Primärkassen?",
+
+      multiple: true,
+
+      answers: [
+        "TK",
+        "IKK",
+        "Barmer",
+        "KKH",
+        "AOK",
+        "DAK"
+      ],
+
+      correct: [1,4]
+    },
+
+    {
+      question:
+      "Zu welcher Kassengruppe gehört die DAK?",
+
+      multiple: false,
+
+      answers: [
+        "PKV",
+        "Regionalkasse",
+        "Sonstige Kostenträger",
+        "vdek",
+        "Berufsgenossenschaft"
+      ],
+
+      correct: [3]
+    },
+
+
+
+
+     {
+  question:
+  "Welche Daten gehören zu den Stammdaten der Patienten?",
+
+  multiple: false,
+
+  answers: [
+    "Name, Adresse, Geburtsdatum und Befund",
+    "Name, Adresse, Geburtsdatum und Telefonnummer",
+    "Name, Adresse, Geburtsdatum und Diagnose",
+    "Name, Adresse, Geburtsdatum und Behandlung",
+    "Name, Adresse, Befund und Diagnose"
+  ],
+
+  correct: [1]
+},
+
+{
+  question:
+  "Welche Verwaltungstätigkeit ist bei der Patientenaufnahme nicht erforderlich?",
+
+  multiple: false,
+
+  answers: [
+    "Anamneseerhebung",
+    "eGK einlesen",
+    "Patientenakte anlegen",
+    "Datenschutzerklärung unterschreiben lassen",
+    "Impfpass digitalisieren"
+  ],
+
+  correct: [4]
+},
+
+{
+  question:
+  "Wofür steht die Abkürzung ZBV?",
+
+  multiple: false,
+
+  answers: [
+    "Zahnärztlicher Bundesverband",
+    "Zahnärztlicher Bezirksverband",
+    "Zahnärzte-Bayern-Verbund",
+    "Zahnärztliche Bundesvereinigung",
+    "Zahnbeschwerden-Verband"
+  ],
+
+  correct: [1]
+},
+
+{
+  question:
+  "Wofür steht die Abkürzung KZVB?",
+
+  multiple: false,
+
+  answers: [
+    "Kassenzahnärztlicher Bundesverband",
+    "Kassenzahnärztlicher Verband Bayern",
+    "Kassenzahnärztliche Vereinigung Bayern",
+    "Kollegiale Zahnärztliche Bundesvereinigung",
+    "Kollegiale Zahnärztliche Vereinigung Bayern"
+  ],
+
+  correct: [2]
+},
+
+{
+  question:
+  "Welche Stelle ist für die Quartalsabrechnung für einen Zahnarzt in Bayern zuständig?",
+
+  multiple: false,
+
+  answers: [
+    "KZV",
+    "KZVB",
+    "BLZK",
+    "ZBV",
+    "BZÄK"
+  ],
+
+  correct: [1]
+},
+
+{
+  question:
+  "Welche beiden Aussagen zum Recall sind richtig?",
+
+  multiple: true,
+
+  answers: [
+    "Recall bedeutet, dass der Patient per Anruf an seinen Termin erinnert wird.",
+    "Recall bedeutet, dass der Patient erinnert wird, wieder einen Termin zu vereinbaren.",
+    "Jeder Patient muss in die Recall-Datei aufgenommen werden.",
+    "Patienten müssen dem Recall zustimmen, damit sie erinnert werden dürfen.",
+    "Jede Zahnarztpraxis muss Recall anbieten.",
+    "Die Zahnarztpraxis entscheidet, welcher Patient in die Recall-Datei aufgenommen wird."
+  ],
+
+  correct: [1,3]
+},
+
+{
+  question:
+  "Welche Aussage zum Datenschutz ist richtig?",
+
+  multiple: false,
+
+  answers: [
+    "Alle Patientendaten werden regelmäßig gesichert.",
+    "Es darf kein Zugriff auf Patientendaten durch Dritte erfolgen.",
+    "Patientendaten müssen lebenslang aufbewahrt werden.",
+    "Datenschutz gilt nur für digitale Daten.",
+    "Alle Patientendaten müssen nach Abschluss der Behandlung gelöscht werden."
+  ],
+
+  correct: [1]
+},
+
+{
+  question:
+  "Was gehört nicht zur Hardware?",
+
+  multiple: false,
+
+  answers: [
+    "Monitor",
+    "Lesegerät für die eGK",
+    "Patientenverwaltungssystem (PVS)",
+    "Drucker",
+    "Maus"
+  ],
+
+  correct: [2]
+},
+
+{
+  question:
+  "Welche beiden Aussagen zum Datenschutzbeauftragten sind richtig?",
+
+  multiple: true,
+
+  answers: [
+    "Jede Praxis muss unabhängig von ihrer Größe eine Datenschutzbeauftragte bestellen",
+    "Nur die Erstkraft darf zur Datenschutzbeauftragten ernannt werden.",
+    "Die Datenschutzbeauftragte achtet auf die Einhaltung der Datenschutzgrundverordnung (DSGVO) und anderer Vorschriften zum Datenschutz.",
+    "Um Datenschutzbeauftragte werden zu können, muss man eine Prüfung vor der Zahnärztekammer ablegen.",
+    "Eine Datenschutzbeauftragte muss immer benannt werden, wenn die Praxis einen Internetzugang hat.",
+    "Die Datenschutzbeauftragte muss die gesetzlichen Regelungen zum Datenschutz kennen und anwenden können."
+  ],
+
+  correct: [2,5]
+},
+
+{
+  question:
+  "Welche beiden Aussagen zum Datenschutz sind falsch?",
+
+  multiple: true,
+
+  answers: [
+    "Eltern dürfen immer die Aufzeichnungen ihrer Kinder einsehen, bis diese 18 Jahre alt sind.",
+    "Ein Patient kann von seinem Zahnarzt Auskunft verlangen, an welche Empfänger seine persönlichen Daten weitergegeben werden.",
+    "Ein Patient kann von seinem Zahnarzt Auskunft über den Zweck der Speicherung seiner persönlichen Daten verlangen.",
+    "Wünscht ein Patient Auskunft über seine persönlichen Daten, dann kann der Zahnarzt ihm in seine Aufzeichnungen Einblick gewähren.",
+    "Bei der Auskunftserteilung über die gespeicherten persönlichen Daten von Patienten handelt es sich um eine Zusatzleistung, die nach GOZ abgerechnet werden kann.",
+    "Ein Patient kann, bis auf wenige Ausnahmen, von seinem Zahnarzt Auskunft über die zu seiner Person gespeicherten Daten verlangen."
+  ],
+
+  correct: [0,4]
+},
+
+{
+  question:
+  "Welche beiden Aussagen zum Umgang mit personenbezogenen Daten gemäß DSGVO sind falsch?",
+
+  multiple: true,
+
+  answers: [
+    "Ein Patient hat das Recht, dass seine Daten berichtigt werden, wenn sie unrichtig sind.",
+    "Personenbezogene Daten dürfen nicht von Auszubildenden eingesehen werden.",
+    "Ein Patient hat das Recht, dass seine Daten berichtigt werden, wenn diese falsch sind.",
+    "Ein Patient hat das Recht, dass auch Verdachtsdiagnosen, die sich nicht bewahrheitet haben, gelöscht werden.",
+    "Personenbezogene Daten dürfen nicht gelöscht werden, wenn die Aufbewahrungsfrist noch läuft.",
+    "Ein Patient hat das Recht, dass seine Daten gelöscht werden, wenn ihre Speicherung nicht erlaubt ist."
+  ],
+
+  correct: [1,3]
+},
+
+{
+  question:
+  "Welche beiden Aussagen zum Datenschutz sind richtig?",
+
+  multiple: true,
+
+  answers: [
+    "Ein gutes Passwort sollte kurz sein.",
+    "Ein sicheres Passwort sollte Sonderzeichen und Großbuchstaben enthalten.",
+    "Ein Passwort sollte über Jahre gleich bleiben.",
+    "Passwörter sollte man hinter dem Monitor notieren.",
+    "Die Speicherung von Passwörtern sollte ausgeschaltet werden.",
+    "Wenn Kolleginnen das Passwort kennen, sollte es geändert werden."
+  ],
+
+  correct: [1,4]
+},
+
+{
+  question:
+  "Welches Verhalten von Manuela ist nicht korrekt?",
+
+  multiple: false,
+
+  answers: [
+    "Manuela bittet einen schwerhörigen Patienten in ein Behandlungszimmer.",
+    "Manuela schützt Daten an der Anmeldung.",
+    "Manuela verweigert Rezeptausgabe ohne Vollmacht.",
+    "Manuela gibt der angeblichen AOK telefonisch Auskunft.",
+    "Manuela beschränkt den Zugriff des Technikers."
+  ],
+
+  correct: [3]
+},
+
+{
+  question:
+  "Welche Aussage über eine digitale Patientendatei ist richtig?",
+
+  multiple: false,
+
+  answers: [
+    "Als Hardware braucht man ein Zahnarztprogramm.",
+    "Als Hardware braucht man PC, Monitor und Drucker.",
+    "In digitalen Karteikarten sind nur Bewegungsdaten.",
+    "Jede Praxis nutzt zusätzlich Papierkarteikarten.",
+    "In digitalen Karteikarten sind nur Stammdaten."
+  ],
+
+  correct: [1]
+},
+
+{
+  question:
+  "Welches Passwort ist sicher?",
+
+  multiple: false,
+
+  answers: [
+    "Abcdefgh",
+    "12345678",
+    "987654321",
+    "Sarah12122008",
+    "IwdPb2223!"
+  ],
+
+  correct: [4]
+},
+
+{
+  question:
+  "Welche beiden Ziele verfolgt der Datenschutz?",
+
+  multiple: true,
+
+  answers: [
+    "Personal- und Patientendaten sicher verwalten.",
+    "Zugriff soll überall möglich sein.",
+    "Recall braucht immer Zustimmung.",
+    "PCs sollen passwortgeschützt sein.",
+    "Risiko eines PC-Ausfalls verringern.",
+    "Patientendaten dürfen für Werbung genutzt werden."
+  ],
+
+  correct: [0,4]
+}
 
   ],
 
